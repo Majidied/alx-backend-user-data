2bae569f0d5cf1295e18f6f200c2e05370a524be
majidied <majidimajidi2003@gmail.com>
2024-07-12 11:00:32 +0100

    chore: Refactor require_auth method to handle excluded_paths correctly

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,16 +12,17 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method to check if auth is required.
         """
-        if path is None:
+        if path is None or excluded_paths is None or excluded_paths == []:
             return True
-
-        if excluded_paths is None or not excluded_paths:
-            return True
-
+        if path in excluded_paths:
+            return False
+        if path[-1] != '/':
+            path += '/'
         for excluded_path in excluded_paths:
+            if excluded_path[-1] != '/':
+                excluded_path += '/'
             if fnmatch.fnmatch(path, excluded_path):
                 return False
-
         return True
 
     def authorization_header(self, request=None) -> str:
