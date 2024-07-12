2bcbd5ed63c8c8183d07a27097a1eff2b1a6e098
majidied <majidimajidi2003@gmail.com>
2024-07-12 17:43:23 +0100

    chore: Refactor authentication methods for improved readability and consistency

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,25 +12,24 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method to check if auth is required.
         """
-        if path is None or excluded_paths is None or excluded_paths == []:
+        if path is None:
             return True
-        if path in excluded_paths:
-            return False
-        if path[-1] != '/':
-            path += '/'
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
         for excluded_path in excluded_paths:
-            if excluded_path[-1] != '/':
-                excluded_path += '/'
             if fnmatch.fnmatch(path, excluded_path):
                 return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
         """ Method to get authorization header.
         """
-        if request is None:
-            return None
-        return request.headers.get('Authorization')
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Method to get user from request.
