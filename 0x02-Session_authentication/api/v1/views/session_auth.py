285fdc28c92c9726dd1282e9085e3d37f05e4470
majidied <majidimajidi2003@gmail.com>
2024-07-13 14:30:53 +0100

    fix style

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -48,6 +48,7 @@ def login():
     response.set_cookie(session_name, session_id)
     return response
 
+
 @app_views.route("/auth_session/logout", methods=["DELETE"], strict_slashes=False)
 def logout():
     """Handle user logout using session authentication.
@@ -65,6 +66,7 @@ def logout():
 
     """
     from api.v1.app import auth
+
     if not auth.destroy_session(request):
         abort(404)
     return jsonify({}), 200
