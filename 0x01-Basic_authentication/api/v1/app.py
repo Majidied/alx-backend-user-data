51b183884295b7d87cceb7c739e8450d15f8952e
majidied <majidimajidi2003@gmail.com>
2024-07-13 08:07:10 +0100

    chore: Refactor authentication methods for improved readability and consistency

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,22 +5,20 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     auth = Auth()
-if auth_type == 'basic_auth':
+elif auth_type == 'basic_auth':
     auth = BasicAuth()
-
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
@@ -28,20 +26,17 @@ def unauthorized(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
-
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """Forbidden handler.
     """
     return jsonify({"error": "Forbidden"}), 403
 
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
-
 
 @app.before_request
 def authenticate_user():
@@ -61,8 +56,7 @@ def authenticate_user():
             if user is None:
                 abort(403)
 
-
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
+    port = int(getenv("API_PORT", "5000"))
     app.run(host=host, port=port)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,13 +15,11 @@ class Auth:
         if path is None:
             return True
 
-        if excluded_paths is None or not excluded_paths:
+        if excluded_paths is None or len(excluded_paths) == 0:
             return True
 
-        for excluded_path in excluded_paths:
-            if fnmatch.fnmatch(path, excluded_path):
-                return False
-
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:
