f5d2b36a5b10d8ed1009f69f541c2bfbe9686ca9
majidied <majidimajidi2003@gmail.com>
2024-07-14 15:50:46 +0100

    Refactor session authentication to improve user retrieval and session handling

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -49,8 +49,8 @@ def login():
     return response
 
 
-@app_views.route("/auth_session/logout", methods=["DELETE"], strict_slashes=
-                 False)
+@app_views.route("/auth_session/logout", methods=["DELETE"],
+                 strict_slashes=False)
 def logout():
     """Handle user logout using session authentication.
 
