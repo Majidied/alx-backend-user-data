51864f48549afe7fc3fe0ff2c6dcd0a875f03bfb
majidied <majidimajidi2003@gmail.com>
2024-07-16 23:26:20 +0100

    feat: Register a new user in the main file

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python3
 from bcrypt import hashpw, gensalt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
     """Hash a password"""
-    return hashpw(password.encode('utf-8'), gensalt())
+    return hashpw(password.encode("utf-8"), gensalt())
+
+
+from db import DB
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = None
+
+        if user:
+            raise ValueError(f"User {email} already exists")
+        return self._db.add_user(email, _hash_password(password))
