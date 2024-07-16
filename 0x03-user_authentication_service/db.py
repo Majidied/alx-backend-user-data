489df14d963382ee15021fa6a4ed10e258d92792
majidied <majidimajidi2003@gmail.com>
2024-07-16 21:07:46 +0100

    chore: Refactor session authentication to improve user retrieval and session handling

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
 from user import Base, User
 
 
@@ -33,3 +36,24 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by a given attribute"""
+        user_keys = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in user_keys:
+                raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user in the database"""
+        try:
+            user = self._session.query(User).filter_by(id=user_id).first()
+            for key, value in kwargs.items():
+                setattr(user, key, value)
+            self._session.commit()
+        except Exception:
+            raise ValueError
