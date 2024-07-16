5779c5d5af5eab88ffc5e3254d604f7aedda4776
majidied <majidimajidi2003@gmail.com>
2024-07-16 23:57:45 +0100

    Refactor user registration to handle existing users

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -23,8 +23,7 @@ class Auth:
         """Register a user"""
         try:
             user = self._db.find_user_by(email=email)
-            if user:
-                raise ValueError(f"User {email} already exists")
+            raise ValueError(f"User {email} already exists")
         except NoResultFound:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, str(hashed_password))
