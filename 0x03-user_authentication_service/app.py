e96986994ac46505708da5a1b261f0e85a9d538f
majidied <majidimajidi2003@gmail.com>
2024-07-17 02:50:32 +0100

    feat: Add user registration endpoint to the main file

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,5 +17,21 @@ def hello() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """POST /users
+    Register a user
+    Return:
+      - a message
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
