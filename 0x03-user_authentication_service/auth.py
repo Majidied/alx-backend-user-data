caf7a5a08ac3cedcbc43ffaec640d445c47ddd3c
majidied <majidimajidi2003@gmail.com>
2024-07-17 10:29:53 +0100

    feat: Improve password comparison in Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ from bcrypt import hashpw, gensalt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from hmac import compare_digest
 
 
 def _hash_password(password: str) -> str:
@@ -52,8 +53,9 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return user.hashed_password == hashpw(
-                password.encode("utf-8"), user.hashed_password
+            return compare_digest(
+                user.hashed_password,
+                hashpw(password.encode("utf-8"), user.hashed_password),
             )
         except NoResultFound:
             return False
