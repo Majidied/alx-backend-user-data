651b8787c958f319d810f7e07a460f944dbb7f00
majidied <majidimajidi2003@gmail.com>
2024-07-18 12:30:42 +0100

    chore: Add password reset functionality to Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -104,3 +104,20 @@ class Auth:
             return None
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """reset password of user
+
+        Args:
+            email (str): email correspond to user
+
+        Returns:
+            str: reset token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except ValueError:
+            raise ValueError
