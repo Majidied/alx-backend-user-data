77383daf2481a53942cb1eed114f692efe10afb9
majidied <majidimajidi2003@gmail.com>
2024-07-18 17:27:38 +0100

    chore: Update reset_password method to handle invalid reset tokens

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -144,6 +144,7 @@ class Auth:
         if user is None:
             raise ValueError("Invalid reset token")
         self._db.update_user(
-            user.id, hashed_password=_hash_password(password), reset_token=None
-        )
+            user.id,
+            hashed_password=_hash_password(password),
+            reset_token=None)
         return None
