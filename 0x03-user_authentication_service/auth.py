d6c97e5e77529c68348869622063ab4fd9235b29
majidied <majidimajidi2003@gmail.com>
2024-07-18 08:59:35 +0100

    feat: Refactor destroy_session method in Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,7 +91,7 @@ class Auth:
             except NoResultFound:
                 return None
         return None
-    
+
     def destroy_session(self, user_id: int) -> None:
         """destroy session of user having user_id
 
