89e8172f1793f1064c2c7c95147cc13a6ecdebab
majidied <majidimajidi2003@gmail.com>
2024-07-18 17:55:30 +0100

    chore: Refactor password update endpoint to return JSON response

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -105,24 +105,23 @@ def get_reset_password_token() -> str:
 
 
 @app.route("/reset_password", methods=["PUT"], strict_slashes=False)
-def update_password() -> str:
+def update_password() -> 'Response':
     """PUT /reset_password
 
     Return:
         - The user's password updated payload.
     """
-    email = request.form.get("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
-    is_password_changed = False
     try:
+        email = request.form.get("email")
+        reset_token = request.form.get("reset_token")
+        new_password = request.form.get("new_password")
+
         AUTH.update_password(reset_token, new_password)
-        is_password_changed = True
+        response = make_response(
+            jsonify({"email": email, "message": "Password updated"}))
+        return response
     except ValueError:
-        is_password_changed = False
-    if not is_password_changed:
         abort(403)
-    return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == "__main__":
