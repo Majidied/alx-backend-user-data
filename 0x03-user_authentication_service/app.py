2f5fbe4d9e119c3e97cfd4cd0ed4457f4e3a6cfa
majidied <majidimajidi2003@gmail.com>
2024-07-18 08:37:15 +0100

    feat: Improve login functionality and session creation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -34,21 +34,18 @@ def users() -> str:
 
 
 @app.route("/sessions", methods=["POST"], strict_slashes=False)
-def login():
+def login() -> str:
     """POST /sessions
-
-    Returns:
-        str: JSON payload
+    Return:
+        - The account login payload.
     """
-    email = request.form.get("email")
-    password = request.form.get("password")
-    if AUTH.valid_login(email, password):
-        session_id = AUTH.create_session(email)
-        response = make_response(jsonify({"email": email, "message": "logged in"}))
-        response.set_cookie("session_id", session_id)
-        return response
-    else:
+    email, password = request.form.get("email"), request.form.get("password")
+    if not AUTH.valid_login(email, password):
         abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
