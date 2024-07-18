697e16419cc59e12e97a595d5ddbc7bb122ff85a
majidied <majidimajidi2003@gmail.com>
2024-07-18 10:52:12 +0100

    chore: Update strict_slashes parameter in /sessions route to True

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -33,7 +33,7 @@ def users() -> str:
         return jsonify({"message": "email already registered"})
 
 
-@app.route("/sessions", methods=["POST"], strict_slashes=False)
+@app.route("/sessions", methods=["POST"], strict_slashes=True)
 def login() -> Response:
     """POST /sessions
 
